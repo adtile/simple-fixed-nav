1b16a12952a98e68d452d1b3226e2c6acf7fb30d
Pablo Garcia <pgarciacamou@gmail.com>
2016-07-19 12:39:32 -0700

    added ejs

--- gulpfile.js
+++ gulpfile.js
@@ -2,44 +2,63 @@ var path = require('path')
     ,gulp =  require('gulp')
     ,babel = require('gulp-babel')
     ,connect =  require('gulp-connect')
-    ,less =     require("gulp-less")
+    ,less = require("gulp-less")
+    ,ejs = require("gulp-ejs")
+    ,clean = require('gulp-clean')
     ,src = "./src"
     ,dest = "./dist";
- 
-gulp.task('connect', function() {
-  connect.server({
-    root: src,
-    port: 8080,
-    livereload: true
-  });
-});
 
-gulp.task("less", function () {
-  gulp.src(path.join(src, "styles/**/*.less"))
+function lessTask() {
+  gulp.src([path.join(src, "styles/**/*.less"), '!' + path.join(src, "styles/**/_*.less")])
     .pipe(less())
     .pipe(gulp.dest(path.join(dest, "styles")))
     .pipe(connect.reload());
-});
+}
 
-gulp.task('html', function () {
-  gulp.src(path.join(src, "*.html"))
+function ejsTask() {
+  gulp.src([path.join(src, "*.ejs"), '!' + path.join(src, "_*.ejs")])
+    .pipe(ejs({}, {ext: '.html'}))
     .pipe(gulp.dest(dest))
     .pipe(connect.reload());
-});
 
-gulp.task('js', function () {
-  gulp.src(path.join(src, "scripts/*.js"))
+  gulp.src([path.join(src, "partials/*.ejs"), '!' + path.join(src, "partials/_*.ejs")])
+    .pipe(ejs({}, {ext: '.html'}))
+    .pipe(gulp.dest(path.join(dest, 'partials')))
+    .pipe(connect.reload());
+}
+
+function es6Task() {
+  gulp.src([path.join(src, "scripts/*.js"), '!' + path.join(src, "scripts/_*.js")])
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest(path.join(dest, "scripts")))
     .pipe(connect.reload());
-});
+}
 
-gulp.task('watch', function () {
-  gulp.watch(['./src/**/*.html'], ['html']);
-  gulp.watch(['./src/scripts/**/*.js'], ['js']);
+function cleanTask() {
+  gulp.src(dest, {read: false}).pipe(clean({force: true}));
+}
+
+function connectTask() {
+  connect.server({
+    root: dest,
+    port: 8080,
+    livereload: true
+  });
+}
+
+function watchTask() {
+  gulp.watch(['./src/*.ejs', './src/partials/**/*.ejs'], ['ejs']);
+  gulp.watch(['./src/scripts/**/*.js'], ['es6']);
   gulp.watch(['./src/styles/**/*.less'], ['less']);
-});
- 
-gulp.task('default', ['less', 'js', 'html', 'watch', 'connect']);
+}
+
+gulp.task('connect', connectTask);
+gulp.task('less', lessTask);
+gulp.task('ejs', ejsTask);
+gulp.task('es6', es6Task);
+gulp.task('clean', cleanTask);
+gulp.task('dist', ['ejs', 'es6', 'less'])
+gulp.task('watch', watchTask);
+gulp.task('default', ['dist', 'watch', 'connect']);
